74dbf53536a94b2b06c73398e006f16a7dcd5ea7
PIYoung <49637184+PIYoung@users.noreply.github.com>
2022-10-26 19:39:49 +0900

    Update styled.tsx

--- src/components/organisms/BrowseByCategory/styled.tsx
+++ src/components/organisms/BrowseByCategory/styled.tsx
@@ -36,6 +36,12 @@ export const CategoryContainer = styled.div`
 export const Category = styled.div`
   cursor: pointer;
 
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+  border-radius: 10px;
+
   & > div:nth-of-type(1) {
     width: 440px;
     height: 224px;
