6a2312c54e71818ab4f160646b665046664957eb
PIYoung <dlsdudg15@naver.com>
2022-11-05 20:48:53 +0900

    update user profile

--- src/components/organisms/MarketDetailContent/index.tsx
+++ src/components/organisms/MarketDetailContent/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 import * as S from './styled';
 import banner from '../../../assets/images/banner.avif';
 import profile from '../../../assets/images/profile1.png';
+import tree1 from '../../../assets/images/tree1.png';
+import tree2 from '../../../assets/images/tree2.png';
+import wood from '../../../assets/images/wood.png';
+import { MEDIA } from '../../../constants';
+
+const { minWidth } = MEDIA;
 
 export default function MarketDetailContent() {
+  const isDesktop = useMediaQuery({ minWidth });
+
+  return isDesktop ? <Desktop /> : <Mobile />;
+}
+
+function Desktop() {
   return (
     <S.Container>
       <S.Banner>
@@ -25,13 +38,50 @@ export default function MarketDetailContent() {
               <div>컬렉션 정보</div>
               <div>
                 <div>에셋</div>
-                <div>8</div>
+                <div>2</div>
               </div>
             </S.CollectionInfo>
           </S.Profile>
         </S.Info>
-        <S.Items></S.Items>
+        <S.Items>
+          <S.Item>
+            <S.ItemImg>
+              <img src={tree1} alt='tree' />
+            </S.ItemImg>
+            <S.ItemName>나무 이름</S.ItemName>
+            <S.ItemCount>1 / 1</S.ItemCount>
+            <S.ItemPrice>
+              <div>
+                <img src={wood} alt='wood' />
+              </div>
+              <div>
+                <div>3150.00</div>
+                <div>2923.03 USD</div>
+              </div>
+            </S.ItemPrice>
+          </S.Item>
+          <S.Item>
+            <S.ItemImg>
+              <img src={tree2} alt='tree' />
+            </S.ItemImg>
+            <S.ItemName>나무 이름</S.ItemName>
+            <S.ItemCount>1 / 1</S.ItemCount>
+            <S.ItemPrice>
+              <div>
+                <img src={wood} alt='wood' />
+              </div>
+              <div>
+                <div>3150.00</div>
+                <div>2923.03 USD</div>
+              </div>
+            </S.ItemPrice>
+          </S.Item>
+        </S.Items>
       </S.Content>
     </S.Container>
   );
+}
+
+function Mobile() {
+  return <div>Mobile</div>;
 }
--- src/components/organisms/MarketDetailContent/styled.ts
+++ src/components/organisms/MarketDetailContent/styled.ts
@@ -111,5 +111,92 @@ export const CollectionInfo = styled.div`
 `;
 
 export const Items = styled.div`
-  min-height: 800px;
+  min-height: calc(100vh - 500px);
+  margin-top: 24px;
+
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 220px);
+  justify-content: center;
+  grid-gap: 20px 0;
+`;
+
+export const Item = styled.div`
+  width: 204px;
+  height: 270px;
+
+  border: 4px solid transparent;
+  border-radius: 10px;
+
+  background-image: linear-gradient(#f3f3f3, #f3f3f3),
+    linear-gradient(
+      196.75deg,
+      rgba(246, 114, 128, 0.6) 0.02%,
+      rgba(108, 91, 123, 0.4) 0.03%,
+      rgba(246, 114, 128, 0.6) 82.49%,
+      rgba(246, 114, 128, 0.6) 82.49%
+    );
+  background-origin: border-box;
+  background-clip: content-box, border-box;
+`;
+
+export const ItemImg = styled.div`
+  display: flex;
+  justify-content: center;
+  & > img {
+    height: 160px;
+    width: 164px;
+    object-fit: contain;
+    filter: brightness(1.2);
+  }
+`;
+
+export const ItemName = styled.div`
+  width: 100%;
+
+  margin-left: 16px;
+
+  font-size: 18px;
+  font-weight: 500;
+
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+export const ItemCount = styled.div`
+  margin-left: 16px;
+
+  font-size: 14px;
+  font-weight: 400;
+  color: var(--color-text);
+  opacity: 0.7;
+`;
+
+export const ItemPrice = styled.div`
+  margin-left: 16px;
+
+  display: flex;
+
+  & > div:nth-of-type(1) {
+    margin-top: 3px;
+    margin-right: 8px;
+
+    width: 24px;
+    height: 24px;
+  }
+
+  & > div:nth-of-type(2) {
+    & > div:nth-of-type(1) {
+      font-size: 18px;
+      font-weight: 600;
+      color: #35477d;
+    }
+
+    & > div:nth-of-type(2) {
+      font-size: 14px;
+      font-weight: 400;
+      color: var(--color-text);
+      opacity: 0.7;
+    }
+  }
 `;
