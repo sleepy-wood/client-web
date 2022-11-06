5cba6be84c034f73878839a1fb2bef5f15688d15
PIYoung <dlsdudg15@naver.com>
2022-11-06 21:08:28 +0900

    update item detail content style

--- src/components/organisms/ItemDetailContent/index.tsx
+++ src/components/organisms/ItemDetailContent/index.tsx
@@ -1,9 +1,11 @@
 import React, { useCallback } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { FaShoppingBasket, FaHeart } from 'react-icons/fa';
 
 import * as C from '../../../constants';
 import * as S from './styled';
+import profile from '../../../assets/images/profile1.png';
 import tree1 from '../../../assets/images/tree1.png';
 import tree2 from '../../../assets/images/tree2.png';
 import wood from '../../../assets/images/wood.png';
@@ -42,7 +44,9 @@ function Desktop() {
         <S.AssetInfo>
           <S.AssetName>나무이름</S.AssetName>
           <S.AssetSeller>
-            <div>프로필</div>
+            <div>
+              <img src={profile} alt='profile' />
+            </div>
             <div>
               <div>크리에이터</div>
               <div>User Nickname</div>
@@ -53,6 +57,7 @@ function Desktop() {
             </div>
           </S.AssetSeller>
           <div>
+            <S.AssetCurrentPrice>CURRENT PRICE</S.AssetCurrentPrice>
             <S.AssetPrice>
               <div>
                 <img src={wood} alt='wood' />
@@ -63,12 +68,23 @@ function Desktop() {
               </div>
             </S.AssetPrice>
             <S.AssetButtonContainer>
-              <div>장바구니 담기</div>
-              <div>위시리스트 추가</div>
+              <div>지금구매</div>
+              <div>
+                <FaShoppingBasket size={24} />
+              </div>
+              <div>
+                <FaHeart size={24} />
+              </div>
             </S.AssetButtonContainer>
-            <S.AssetDescription>나무에대 한 설명을 막 쓰면 됨</S.AssetDescription>
+            <S.AssetDescription>
+              선거와 국민투표의 공정한 관리 및 정당에 관한 사무를 처리하기 위하여 선거관리위원회를
+              둔다. 국회는 국정을 감사하거나 특정한 국정사안에 대하여 조사할 수 있으며, 이에 필요한
+              서류의 제출 또는 증인의 출석과 증언이나 의견의 진술을 요구할 수 있다. 공무원의 신분과
+              정치적 중립성은 법률이 정하는 바에 의하여 보장된다. 법관이 중대한 심신상의 장해로
+              직무를 수행할 수 없을 때에는 법률이 정하는 바에 의하여 퇴직하게 할 수 있다.
+            </S.AssetDescription>
+            <S.SemiTitle>이 크리에이터의 NFT 더 알아보기</S.SemiTitle>
             <S.ExtraAssets>
-              <div>이 크리에이터의 NFT 더 알아보기</div>
               {[1, 2].map((_, index) => (
                 <React.Fragment>
                   <S.ExtraAsset key={index + 1} onClick={moveToPath.bind(null, C.PATH.ITEM_DETAIL)}>
@@ -106,8 +122,8 @@ function Desktop() {
                 </React.Fragment>
               ))}
             </S.ExtraAssets>
+            <S.SemiTitle>추천 상품</S.SemiTitle>
             <S.ExtraAssets>
-              <div>추천 상품</div>
               {[1, 2].map((_, index) => (
                 <React.Fragment>
                   <S.ExtraAsset key={index + 1} onClick={moveToPath.bind(null, C.PATH.ITEM_DETAIL)}>
--- src/components/organisms/ItemDetailContent/styled.ts
+++ src/components/organisms/ItemDetailContent/styled.ts
@@ -41,31 +41,204 @@ export const AssetImg = styled.div`
 
 export const AssetInfo = styled.div`
   width: calc(100% - 400px);
+
+  padding: 0 48px;
 `;
 
-export const AssetName = styled.div``;
+export const AssetName = styled.h2`
+  width: 100%;
+  overflow: hidden;
 
-export const AssetSeller = styled.div``;
+  font-size: 36px;
+  font-weight: 600;
 
-export const AssetPrice = styled.div``;
+  margin-bottom: 16px;
+`;
 
-export const AssetButtonContainer = styled.div``;
+export const AssetSeller = styled.div`
+  display: flex;
+  align-items: center;
 
-export const AssetDescription = styled.div``;
+  width: 100%;
+  padding: 20px 25px;
+
+  border: 2px solid #c06c84;
+  border-radius: 16px;
+
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+
+  & > div:not(:first-child) {
+    margin-left: 16px;
+  }
+
+  & > div:nth-of-type(1) {
+    width: 60px;
+    height: 60px;
+
+    overflow: hidden;
+    border-radius: 50%;
+
+    & > img {
+      width: 100%;
+      height: 100%;
+
+      object-fit: cover;
+    }
+  }
+
+  & > div:nth-of-type(2) {
+    width: 120px;
+
+    & > div:nth-of-type(1) {
+      font-size: 12px;
+      color: rgb(4, 17, 29);
+      opacity: 0.6;
+    }
+
+    & > div:nth-of-type(2) {
+      font-size: 14px;
+
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+  }
+
+  & > div:nth-of-type(3) {
+    & > div:nth-of-type(1) {
+      font-size: 12px;
+      color: rgb(4, 17, 29);
+      opacity: 0.6;
+    }
+
+    & > div:nth-of-type(2) {
+      font-size: 14px;
+      color: #00adef;
+
+      cursor: pointer;
+
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+  }
+`;
+
+export const AssetCurrentPrice = styled.div`
+  margin-top: 20px;
+  margin-left: 16px;
+`;
+
+export const AssetPrice = styled.div`
+  margin-left: 16px;
+
+  display: flex;
+
+  & > div:nth-of-type(1) {
+    margin-top: 3px;
+    margin-right: 8px;
+
+    width: 34px;
+    height: 34px;
+  }
+
+  & > div:nth-of-type(2) {
+    & > div:nth-of-type(1) {
+      font-size: 24px;
+      font-weight: 600;
+      color: #35477d;
+    }
+
+    & > div:nth-of-type(2) {
+      font-size: 14px;
+      font-weight: 400;
+      color: var(--color-text);
+      opacity: 0.7;
+    }
+  }
+`;
+
+export const AssetButtonContainer = styled.div`
+  margin-top: 20px;
+
+  display: flex;
+  align-items: center;
+
+  & > div:nth-of-type(1) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 100%;
+    height: 40px;
+
+    font-size: 16px;
+    font-weight: 500;
+
+    color: var(--color-text);
+
+    cursor: pointer;
+
+    border: 1px solid black;
+    border-radius: 12px;
+
+    background: rgba(255, 255, 255, 0.5);
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    backdrop-filter: blur(4px);
+    -webkit-backdrop-filter: blur(4px);
+    border: 1px solid rgba(255, 255, 255, 0.18);
+  }
+
+  & > div:not(:first-child) {
+    width: 40px;
+    height: 40px;
+
+    margin-left: 8px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    cursor: pointer;
+
+    border-radius: 50%;
+
+    background: rgba(255, 255, 255, 0.5);
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    backdrop-filter: blur(4px);
+    -webkit-backdrop-filter: blur(4px);
+    border: 1px solid rgba(255, 255, 255, 0.18);
+  }
+`;
+
+export const AssetDescription = styled.div`
+  margin-top: 48px;
+`;
+
+export const SemiTitle = styled.div`
+  margin-top: 16px;
+
+  font-size: 15px;
+  color: rgb(4, 17, 29);
+  opacity: 0.6;
+`;
 
 export const ExtraAssets = styled.div`
-  min-height: calc(100vh - 500px);
-  margin-top: 24px;
+  margin-top: 12px;
+  margin-bottom: 16px;
 
   display: grid;
-  grid-template-columns: repeat(auto-fill, 220px);
-  justify-content: center;
-  grid-gap: 20px 0;
+  grid-template-columns: repeat(auto-fill, 160px);
+  justify-content: space-between;
+  grid-gap: 14px 0;
 `;
 
 export const ExtraAsset = styled.div`
-  width: 204px;
-  height: 270px;
+  width: 150px;
+  height: 186px;
 
   border: 4px solid transparent;
   border-radius: 10px;
@@ -88,8 +261,8 @@ export const ExtraAssetImg = styled.div`
   display: flex;
   justify-content: center;
   & > img {
-    height: 160px;
-    width: 164px;
+    height: 90px;
+    width: 94px;
     object-fit: contain;
     filter: brightness(1.2);
   }
@@ -100,7 +273,7 @@ export const ExtraAssetName = styled.div`
 
   margin-left: 16px;
 
-  font-size: 18px;
+  font-size: 14px;
   font-weight: 500;
 
   white-space: nowrap;
@@ -111,7 +284,7 @@ export const ExtraAssetName = styled.div`
 export const ExtraAssetCount = styled.div`
   margin-left: 16px;
 
-  font-size: 14px;
+  font-size: 10px;
   font-weight: 400;
   color: var(--color-text);
   opacity: 0.7;
@@ -132,13 +305,13 @@ export const ExtraAssetPrice = styled.div`
 
   & > div:nth-of-type(2) {
     & > div:nth-of-type(1) {
-      font-size: 18px;
+      font-size: 14px;
       font-weight: 600;
       color: #35477d;
     }
 
     & > div:nth-of-type(2) {
-      font-size: 14px;
+      font-size: 10px;
       font-weight: 400;
       color: var(--color-text);
       opacity: 0.7;
