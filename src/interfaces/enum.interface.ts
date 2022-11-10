d6292ffaabed2f8e5440981dea00b31bade5d20d
PIYoung <dlsdudg15@naver.com>
2022-11-10 19:22:52 +0900

    update interface

---
+++ src/interfaces/enum.interface.ts
@@ -0,0 +1,27 @@
+export enum AttachFileType {
+  Temp = 'Temp',
+  Static = 'Static',
+  Secure = 'Secure',
+}
+
+export enum ProductType {
+  Emoticon = 'Emoticon',
+  Prop = 'Prop',
+}
+
+export enum ContextType {
+  Request = 'Request',
+  WebSocket = 'WebSocket',
+  Unknown = 'Unknown',
+}
+
+export enum UserType {
+  None = 'None',
+  Guest = 'Guest',
+  Kakao = 'Kakao',
+}
+
+export enum Payment {
+  Cash,
+  Card,
+}
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,6 +1,9 @@
 //@index('./*.ts', f => f.path !== `./index` ? `export * from '${f.path}';` : "")
 export * from './common.interface';
+export * from './entity.interface';
+export * from './enum.interface';
 export * from './error.interface';
 export * from './ethereum.interface';
 export * from './response.interface';
+export * from './sleep.interface';
 export * from './user.interface';
