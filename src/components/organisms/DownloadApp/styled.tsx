d0c77ecd24af2862ad92c99583c3052e95182f7e
PIYoung <dlsdudg15@naver.com>
2022-11-11 17:24:00 +0900

    Update styled.tsx

--- src/components/organisms/DownloadApp/styled.tsx
+++ src/components/organisms/DownloadApp/styled.tsx
@@ -11,12 +11,13 @@ export const Container = styled.div`
 `;
 
 export const Main = styled.div`
+  color: #5f6da1;
+
   & > div:nth-of-type(1) {
     font-weight: 700;
     font-size: 40px;
     display: flex;
     align-items: center;
-    color: var(--color-palette3);
     text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     margin-bottom: 12px;
     padding-left: 8px;
@@ -27,7 +28,6 @@ export const Main = styled.div`
     font-size: 24px;
     display: flex;
     align-items: center;
-    color: var(--color-palette3);
     text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     margin-bottom: 12px;
     padding-left: 8px;
