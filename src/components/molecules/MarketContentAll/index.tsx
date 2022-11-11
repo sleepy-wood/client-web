0661b4282962fce236366f81ce25f96d5a44900f
PIYoung <dlsdudg15@naver.com>
2022-11-11 14:00:14 +0900

    Update index.tsx

--- src/components/molecules/MarketContentAll/index.tsx
+++ src/components/molecules/MarketContentAll/index.tsx
@@ -40,96 +40,47 @@ function Desktop() {
         <h1>NFT 마켓플레이스에 오신 것을 환영합니다.</h1>
         <p>여기에서 크리에이터의 에셋을 검색하고 ETH로 구매하여 App에서 사용할 수 있습니다.</p>
       </S.Header>
-      <S.ContentContainer>
-        <S.ContentContainerHeader>
-          <div>컬렉션</div>
-          <div>(100)</div>
-        </S.ContentContainerHeader>
-        <S.CardContainer>
-          {[1, 2, 3, 4, 5].map((_, index) => (
-            <S.Card key={index} onClick={moveToPath.bind(null, C.PATH.MARKET_DETAIL)}>
+      {[
+        ['컬렉션', 'COLLECTION'],
+        ['이모티콘', 'EMOTICON'],
+        ['꽃', 'FLOWER'],
+        ['식물', 'PLANTS'],
+        ['버섯', 'MUSHROOM'],
+        ['바위', 'ROCK'],
+        ['나무 소품', 'WOODEN'],
+        ['라이트', 'LIGHT'],
+      ].map(([item, category], index) => (
+        <S.ContentContainer>
+          <S.ContentContainerHeader key={index}>
+            <div>{item}</div>
+            <div>(100)</div>
+          </S.ContentContainerHeader>
+          <S.CardContainer>
+            {[1, 2, 3, 4, 5].map((_, index) => (
+              <S.Card key={index} onClick={moveToPath.bind(null, C.PATH.MARKET_DETAIL)}>
+                <div>
+                  <img src={beauty} alt='beauty' />
+                </div>
+                <div>
+                  <img src={profile1} alt='profile' />
+                </div>
+                <div>
+                  <div>User Nickname</div>
+                  <div>8 에셋</div>
+                </div>
+              </S.Card>
+            ))}
+          </S.CardContainer>
+          <S.More>
+            <div onClick={moveToPath.bind(null, C.PATH.MARKET[category])}>
+              <div>더보기</div>
               <div>
-                <img src={beauty} alt='beauty' />
+                <FaRegArrowAltCircleRight />
               </div>
-              <div>
-                <img src={profile1} alt='profile' />
-              </div>
-              <div>
-                <div>User Nickname</div>
-                <div>8 에셋</div>
-              </div>
-            </S.Card>
-          ))}
-        </S.CardContainer>
-        <S.More>
-          <div onClick={moveToPath.bind(null, '')}>
-            <div>더보기</div>
-            <div>
-              <FaRegArrowAltCircleRight />
             </div>
-          </div>
-        </S.More>
-      </S.ContentContainer>
-      <S.ContentContainer>
-        <S.ContentContainerHeader>
-          <div>랜드소품</div>
-          <div>(100)</div>
-        </S.ContentContainerHeader>
-        <S.CardContainer>
-          {[1, 2, 3, 4, 5].map((_, index) => (
-            <S.Card key={index}>
-              <div>
-                <img src={beauty} alt='beauty' />
-              </div>
-              <div>
-                <img src={profile1} alt='profile' />
-              </div>
-              <div>
-                <div>User Nickname</div>
-                <div>8 에셋</div>
-              </div>
-            </S.Card>
-          ))}
-        </S.CardContainer>
-        <S.More>
-          <div onClick={moveToPath.bind(null, '')}>
-            <div>더보기</div>
-            <div>
-              <FaRegArrowAltCircleRight />
-            </div>
-          </div>
-        </S.More>
-      </S.ContentContainer>
-      <S.ContentContainer>
-        <S.ContentContainerHeader>
-          <div>이모티콘</div>
-          <div>(100)</div>
-        </S.ContentContainerHeader>
-        <S.CardContainer>
-          {[1, 2, 3, 4, 5].map((_, index) => (
-            <S.Card key={index}>
-              <div>
-                <img src={beauty} alt='beauty' />
-              </div>
-              <div>
-                <img src={profile1} alt='profile' />
-              </div>
-              <div>
-                <div>User Nickname</div>
-                <div>8 에셋</div>
-              </div>
-            </S.Card>
-          ))}
-        </S.CardContainer>
-        <S.More>
-          <div onClick={moveToPath.bind(null, '')}>
-            <div>더보기</div>
-            <div>
-              <FaRegArrowAltCircleRight />
-            </div>
-          </div>
-        </S.More>
-      </S.ContentContainer>
+          </S.More>
+        </S.ContentContainer>
+      ))}
     </S.Container>
   );
 }
