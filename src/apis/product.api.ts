539601446fa275c51c666de15a47fa9d3adf99d0
PIYoung <dlsdudg15@naver.com>
2022-11-12 10:14:09 +0900

    update product apis

--- src/apis/product.api.ts
+++ src/apis/product.api.ts
@@ -6,6 +6,22 @@ import { callRequest } from '../utils';
 const { v1 } = C.APIs;
 
 export const product = {
+  async findAll(params: I.FindAllProductQuery): Promise<[[I.Product[], number], E.HttpException]> {
+    try {
+      const { method, url } = v1.product.findAll;
+      const { result, data, count } = await callRequest<I.RowResponse<I.Product>>({
+        method,
+        url,
+        params,
+      });
+
+      return [[data, count], null];
+    } catch (error) {
+      const { data: _data, status } = error.response;
+
+      return [null, new E.HttpException(_data, status)];
+    }
+  },
   async findFiveByCategory(): Promise<[[I.User[], string][], E.HttpException]> {
     try {
       const { method, url } = v1.product.findFiveByCategory;
