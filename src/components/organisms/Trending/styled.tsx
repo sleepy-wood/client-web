451bef25af90046d4b5301f2c3ab7642d7e4f1a6
PIYoung <dlsdudg15@naver.com>
2022-11-28 16:21:48 +0900

    Update styled.tsx

--- src/components/organisms/Trending/styled.tsx
+++ src/components/organisms/Trending/styled.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  padding: 40px 16px 0;
+  padding: 140px 16px 0;
 `;
 
 export const TitleContainer = styled.div`
